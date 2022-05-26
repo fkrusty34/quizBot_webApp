a4c1e1011b092ebc4b018826d39b361b3d677a2b
fkrusty34 <yarik@shanin.spb.ru>
2022-05-26 20:27:41 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+webapp = window.Telegram.WebApp;
+idu = webapp.initDataUnsafe;
+
+webapp.MainButton.setParams({text: "Отправить", color: "#000000", text_color: "#FFFFFF",
+    is_active: true, is_visible:true});
+
+webapp.ready();
+
+webapp.onEvent("mainButtonClicked", function () {
+    webapp.sendData("TestData");
+})
+
+
+
