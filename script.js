c86c98f8c93b8ef751602d5c81f6db3af4afaedc
fkrusty34 <yarik@shanin.spb.ru>
2022-05-28 12:37:50 +0300

    Some fix

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let webapp = window.Telegram.WebApp;
 webapp.MainButton.setParams({text: "Отправить", color: "#000000", text_color: "#FFFFFF",
     is_active: true, is_visible: true});
 
-webapp.ready();
+//webapp.ready();
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
     webapp.sendData("TestData3");
