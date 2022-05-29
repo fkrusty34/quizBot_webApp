c79c2990412ec494093a8233622dbfee1f996941
fkrusty34 <yarik@shanin.spb.ru>
2022-05-29 19:16:36 +0300

    Fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let webapp = window.Telegram.WebApp;
 
-webapp.MainButton.setParams({text: "Отправить", color: "#000000", text_color: "#FFFFFF",
+webapp.MainButton.setParams({text: "Отправить", color: "#0fff00", text_color: "#ffffff",
     is_active: true, is_visible: true});
 
 //webapp.ready();
