5bf875f9f5c7f3e5856152bec3e13e4f5436685d
fkrusty34 <yarik@shanin.spb.ru>
2022-05-31 13:23:28 +0300

    New adds

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let webapp = window.Telegram.WebApp;
 
-webapp.MainButton.setParams({text: "Отправить", color: "#0fff00", text_color: "#ffffff",
+webapp.MainButton.setParams({text: "Отправить", color: "#64bb00", text_color: "#ffffff",
     is_active: true, is_visible: true});
 
 //webapp.ready();
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
-    let data = document.getElementById("data").value;
+    let chb1 = document.getElementById("chb1").value;
+    let chb2 = document.getElementById("chb2").value;
+    let chb3 = document.getElementById("chb3").value;
+    let comment = document.getElementById("data").value;
+    let data = chb1 + "|" + chb2 + "|" + chb3;
+
+    if (!(comment.trim() === "")) {
+        data += "|" + comment;
+    }
+
     webapp.sendData(data);
 });
 
